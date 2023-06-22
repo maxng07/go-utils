07ac255faf44b7bf5cf1f10a39d722fe8042e761
Singa-Boy <45160135+maxng07@users.noreply.github.com>
2023-06-22 16:44:42 +0800

    Update revint.go

--- reverse/revint.go
+++ reverse/revint.go
@@ -1,7 +1,7 @@
 // Copyright 2023 Max NG. All Rights Reserved!
 package reverse
 
-func reverse(arr []int) (rev []int) {
+func revint(arr []int) (rev []int) {
 	a := len(arr)
 	for i := 1; i <= a; i++ {
 		rev = append(rev, arr[a-i])
