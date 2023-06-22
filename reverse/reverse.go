6c276eb7e96635ea0c702615edf3f877fcbad2d7
Singa-Boy <45160135+maxng07@users.noreply.github.com>
2023-06-22 16:43:18 +0800

    Create reverse.go

---
+++ reverse/reverse.go
@@ -0,0 +1,23 @@
+#Copyright of Max NG. All Rights Reserved!
+package reverse
+
+import "fmt"
+
+func reverse(arr interface{}) {
+  switch arr := arr.(type) {
+	case []int:
+		a := len(arr)
+		rev := []int{}
+		for i := 1; i <= a; i++ {
+      rev = append(rev, arr[a-i])
+		}
+		fmt.Println(rev)
+	case []string:
+		a := len(arr)
+    rev := []string{}
+    for i := 1; i <= a; i++ {
+      rev = append(rev, arr[a-i])
+    }
+    fmt.Println(rev)
+	}
+}
