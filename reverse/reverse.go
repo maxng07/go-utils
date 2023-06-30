792d93b94f7adcb8f4026d7f75d07c7e266869db
Singa-Boy <45160135+maxng07@users.noreply.github.com>
2023-06-30 14:50:15 +0800

    Update reverse.go
    Added an universal reverse function, input interface, return struct.

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -3,6 +3,11 @@ package reverse
 
 import "fmt"
 
+type output struct {
+	num []int
+	str []string
+}
+
 func reverse(arr interface{}) {
   switch arr := arr.(type) {
 	case []int:
@@ -20,4 +25,28 @@ func reverse(arr interface{}) {
     }
     fmt.Println(rev)
 	}
+}
+
+
+func ureverse(arr interface{}) *output {
+  switch arr := arr.(type) {
+  case []int:
+    a := len(arr)
+    rev := []int{}
+    for i := 1; i <= a; i++ {
+      rev = append(rev, arr[a-i])
+    }
+    //fmt.Println(rev)
+		return &output{ num: rev}
+		
+  case []string:
+    a := len(arr)
+    rev := []string{}
+    for i := 1; i <= a; i++ {
+      rev = append(rev, arr[a-i])
+    }
+    //fmt.Println(rev)
+		return &output{ str: rev}
+	}
+	return &output{}
 }
